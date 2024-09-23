71b8ac7273a43ce0246b3683aed9b7630f3a0b1f
theGlitch1 <d.vyassine@gmail.com>
2024-09-23 03:47:39 +0100

    clean branch after review before merging

--- src/modules/project/constants/Summary.ts
+++ src/modules/project/constants/Summary.ts
@@ -1,24 +1,28 @@
 export const summaryText = {
-  title: "Summary",
+  title: 'Summary',
   description: [
-    "This project showcases a scalable and maintainable approach to building a complex frontend application using Vue 3, Vuetify, and TypeScript. The modular structure, dynamic filtering system, and reusable composables demonstrate best practices for developing robust and flexible applications.",
-    "This project exemplifies problem-solving and optimal design choices, making it a strong foundation for any large-scale frontend application.",
+    'This project showcases a scalable and maintainable approach to building a complex frontend application using Vue 3, Vuetify, and TypeScript. The modular structure, dynamic filtering system, and reusable composables demonstrate best practices for developing robust and flexible applications.',
+    'This project exemplifies problem-solving and optimal design choices, making it a strong foundation for any large-scale frontend application.'
   ],
   highlights: [
-    {icon:'mdi-cube-outline' ,text: "Modular architecture for maintainability and scalability."},
-    {icon: 'mdi-filter-variant' ,text: "Dynamic filtering system with composables for reusable logic."},
-    {icon: 'mdi-palette-outline',text: "Vuetify integration for consistent UI design and theming."},
-    {icon: 'mdi-language-typescript',text: "TypeScript for type safety and improved code quality."},
-    {icon: 'mdi-alphabetical',text: "Alias configuration for simplified imports and code organization."}
+    { icon: 'mdi-cube-outline', text: 'Modular architecture for maintainability and scalability.' },
+    {
+      icon: 'mdi-filter-variant',
+      text: 'Dynamic filtering system with composables for reusable logic.'
+    },
+    {
+      icon: 'mdi-palette-outline',
+      text: 'Vuetify integration for consistent UI design and theming.'
+    },
+    {
+      icon: 'mdi-language-typescript',
+      text: 'TypeScript for type safety and improved code quality.'
+    },
+    {
+      icon: 'mdi-alphabetical',
+      text: 'Alias configuration for simplified imports and code organization.'
+    }
   ],
-  lastWord: "I look forward to discussing how these principles and techniques can contribute to your team's success."
-
+  lastWord:
+    "I look forward to discussing how these principles and techniques can contribute to your team's success."
 }
-/* 
-'mdi-check-circle',
-  'mdi-tune',
-  'mdi-code-tags',
-  'mdi-format-list-bulleted',
-  'mdi-layers',
-  'mdi-shield-check'
-*/
